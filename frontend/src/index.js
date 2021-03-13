52347d4560f84497fd10913e16f0f1ca31c7894c
giegeralexandra <giegeralexandra@gmail.com>
2021-03-13 17:23:58 -0500

    added cors gem and fixed fetch request

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,17 @@
-console.log("hello");
+//make a fetch request to get the data from our server
+const rootEl = docuement.getElementById('root)')
+
+fetch('http://localhost:3000/games')
+.then(res => res.json())
+.then(data => renderGames(data));
+
+const renderGames = function (games) {
+    console.log(games);
+
+    games.forEach(game => {
+        rootEl.innerHTML += 
+        <div>
+            <h2>${game.id}</h2>
+        </div>
+    })
+}
